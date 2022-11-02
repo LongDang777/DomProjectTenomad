5d79bb49f2678adf19ee8ee8e21e9a99dcaaa9e9
Long <dangtienlong2020@gmail.com>
2022-11-02 22:47:22 +0700

    clean code

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-import data from "./data.js";
+const data = [
+  {
+    id: 'colorFlipper',
+    href: './Pages/colorFipper/colorFipperSimple.html',
+    img: './img/image01.webp',
+    footer: 'Color Fliper'
+  },
+  {
+    id: 'simpleCounter',
+    href: './Pages/simpleCounter/simpleCounter.html',
+    img: './img/image02.webp',
+    footer: 'Simple Counter'
+  },
+  {
+    id: 'rewiewsQuotes',
+    href: './Pages/rewiewsQuotes/rewiewsQuotes.html',
+    img: './img/image03.webp',
+    footer: 'Rewiews Quotes'
+  },
+  {
+    id: 'navbar',
+    href: './Pages/navbar/navbar.html',
+    img: './img/image04.webp',
+    footer: 'Navbar'
+  }
+]
 
 const items = data.map(item => (
   `<a class='section__item' href=${item.href} target="_blank" key=${item.id}>
@@ -10,5 +35,6 @@ const items = data.map(item => (
     </footer>
   </a>`
 ))
+
 
 document.getElementById('projects').innerHTML = items.join('')
